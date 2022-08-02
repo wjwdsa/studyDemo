ccb7cd50e11ef3287cc53c9a44ef687ad3aa11aa
wds <wjwdsa@163.com>
2022-08-02 17:04:59 +0800

    studyDemo

---
+++ studyDemo/src/main/java/dao/entBdSbFrkTblActiveXnqyMapper.java
@@ -0,0 +1,9 @@
+package dao;
+
+import entity.entBdSbFrkTblActiveXnqy;
+
+public interface entBdSbFrkTblActiveXnqyMapper {
+    int insert(entBdSbFrkTblActiveXnqy record);
+
+    int insertSelective(entBdSbFrkTblActiveXnqy record);
+}
