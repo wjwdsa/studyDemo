b8803b5c7652c49b880bb2eccd45dba738f55ba5
wds <wjwdsa@163.com>
2022-09-26 18:48:14 +0800

    The Second Commit

--- studyDemo/internet/src/main/java/InternetMain.java
+++ studyDemo/internet/src/main/java/InternetMain.java
@@ -1,7 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /******************************
  * @Description Internet启动类
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022-09-26 10:57
  ******************************/
 @EnableAutoConfiguration
-@RestController
 public class InternetMain {
     public static void main(String[] args) {
         SpringApplication.run(InternetMain.class, args);
